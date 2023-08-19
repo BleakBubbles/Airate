e519e315a442729f8b554a34e30e717dc7af26bc
BleakBubbles <macks0554@gmail.com>
2023-08-19 19:06:51 -0400

    Updated profile page

--- profile.php
+++ profile.php
@@ -1,3 +1,14 @@
+<?php
+
+    session_start();
+
+    $mysqli = require __DIR__ . "\\database.php";
+
+    $sql = $mysqli->query(sprintf("SELECT * FROM user WHERE name = '%s'",$_SESSION["logged_in"]));
+    $points = mysqli_fetch_array($sql)
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +17,13 @@
     <title>Airate</title>
 </head>
 <body>
-    <h1>Airate  Profile</h1>
+    <h1>Airate Profile</h1>
+    
+    <?php if (isset($_SESSION["logged_in"])): ?>
+        <p><?php echo $_SESSION["logged_in"];?></p>
+        <p><?php echo $points["points"];?></p>
+    <?php else: ?>
+        <p>Unable to load profile</p>
+    <?php endif; ?>
 </body>
 </html>
