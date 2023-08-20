785c4df9cf0adbf33c9014c90d381c881de3a3e6
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2023-08-20 00:49:49 -0400

    SIZE OF MAP

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let map, infoWindow;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
      center: { lat: 56.1304, lng: -106.3468 },
-     zoom: 9,
+     zoom: 11,
      streetViewControl: false,
      mapTypeControlOptions: {
         style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
