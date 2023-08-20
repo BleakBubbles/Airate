777cebd6db9290949b661d4bfa3b08bcf0dd99e5
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2023-08-19 23:20:31 -0400

    Changed UI

--- report.php
+++ report.php
@@ -28,6 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Airate</title>
+    <link rel="stylesheet" type="text/css" href="./html/style/report.css"/>
 </head>
 <body>
     <h1>Airate Report</h1>
